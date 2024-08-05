5fe21375eaedfdddfd9896624b1a848668c103a1
Nemo55aa <minato.work1@gmail.com>
2024-08-05 22:37:14 +0900

    文字色をﾐﾄﾞﾘに変更

--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,11 @@
+#define strColor_GRN 0x2a00
+#define strColor_WHT 0xff00
+
 void printf(char *str)
 {
 	unsigned short * VideoMemory = (unsigned short*)0xb8000;
 	for(int i = 0; str[i] != '\0'; ++i)
-		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+		VideoMemory[i] = (VideoMemory[i] & strColor_GRN) | str[i];
 
 }
 
